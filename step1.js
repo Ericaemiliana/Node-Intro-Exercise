8632d7ce590d53c22d9a8614f2a4f9c1a7f9fb19
erica pantoja <ericap@marsreturnship.com>
2021-08-12 13:32:13 -0500

    adding step2

--- step1.js
+++ step1.js
@@ -1,7 +1,19 @@
 const fs = require("fs");
-fs.readFile('one.txt', 'utf8', function(err, data){
-    if(err){
-        console.log("ERROR:", err);
+const process = require("process");
+//read the file at path and print it out
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      //handle possible error
+      console.log("ERROR:", err);
+      //kill the process and tel lthe shell it errored
+      process.exit(1);
     }
+    //otherwise sucess
+    console.log(`file contents: ${data}`);
+  });
+  console.log("reading file");
 }
-){}
+//file won't have been read yet at this point
+cat(process.argv[2]);
+console.log(process.argv);
